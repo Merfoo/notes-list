07b37bc626bed9fb608166ca157eee315284c10c
Fauzi Kliman <fauzimerfoo@gmail.com>
2016-12-18 15:34:50 -0800

    Added userbar component with user display name and sign out button

--- components/App.vue
+++ components/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div  id="app">
         <login v-show="!userLoggedIn"></login>
+        <userbar v-if="userLoggedIn"></userbar>
         <toolbar v-if="userLoggedIn"></toolbar>
         <notes-list v-if="userLoggedIn"></notes-list>
         <editor v-if="userLoggedIn"></editor>
@@ -9,6 +10,7 @@
 
 <script>
 import Login from "./Login.vue"
+import Userbar from "./Userbar.vue"
 import Toolbar from "./Toolbar.vue"
 import NotesList from "./NotesList.vue"
 import Editor from "./Editor.vue"
@@ -16,6 +18,7 @@ import Editor from "./Editor.vue"
 export default {
     components: {
         Login,
+        Userbar,
         Toolbar,
         NotesList,
         Editor
---
+++ components/Userbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="userbar">
+        <h3 id="user-display-name">{{ user.displayName }}</h3>
+        <button type="button" id="btn-sign-out" clas
+            s="btn btn-default"
+            @click="signOut">Sign out</button>
+    </div>
+</template>
+
+<script>
+import firebase from "firebase"
+
+export default {
+    methods: {
+        signOut(){
+            firebase.auth().signOut()
+        }
+    },
+    computed: {
+        user(){
+            return this.$store.getters.user
+        }
+    }
+}
+</script>
